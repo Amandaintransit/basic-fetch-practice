e6493a6112c79b5b189539ea04d06abb0ba5c782
amanda-spears <amanda_spears@hotmail.com>
2025-10-31 22:56:21 -0400

    fetch is working

--- fetch.js
+++ fetch.js
@@ -1,19 +1,12 @@
-fetch("https://github.com/dan-collins-dev/dummy-data-fetching-repo/blob/main/data/users.json")
+fetch("https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json")
 
-.then(response => {
-    if(!response.ok){
-        throw new Error("could not find");
-    }
-    return response;
-})
+.then(response => response.json()) 
+.then(data => console.log(data))
 
+.catch(error => console.error('Error:', error));
+/*
 .then(data => console.log(data.firstName))
 .catch(error => console.error(error));
-    
-let user = {
-    id: 0,
-    firstName: " ",
-    lastName: " ",
-    email: " ",
-    yearsEmployed: 0
+
 }
+*/
