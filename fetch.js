d125b3940fe9df95bd3a1eb5fcca0ef6c6249b8e
amanda-spears <amanda_spears@hotmail.com>
2025-11-01 20:13:18 -0400

    working cards

--- fetch.js
+++ fetch.js
@@ -1,12 +1,29 @@
-fetch("https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json")
 
-.then(response => response.json()) 
-.then(data => console.log(data))
+ 
 
-.catch(error => console.error('Error:', error));
-/*
-.then(data => console.log(data.firstName))
-.catch(error => console.error(error));
+const UserUrl = "https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json";
+function createCards() {
+fetch(UserUrl)
+    .then(response => response.json())
+
+    .then (value => {
+        const cardContainer = document.getElementById("card-container");
+        value.forEach(user => {
+            const card = document.createElement('div');
+            card.className = 'card';
+
+            card.innerHTML = `
+            <p>${user.firstName} ${user.lastName}</p>
+            <p>${user.email}</p>
+            <p>${user.companyName}</p>
+            <p>${user.yearsEmployed}</p>
+            `;
+
+            cardContainer.appendChild(card);
+        });
+    })
+    .catch(error => console.error('Error', error));
 
 }
-*/
+
+createCards();
